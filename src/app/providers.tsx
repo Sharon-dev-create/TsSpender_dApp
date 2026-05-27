ba8fa072fd678a6eecabc4abe0ad42eb87f03553
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-05-28 00:47:11 +0100

    commiting

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,17 +4,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import {type ReactNode } from "react"
 import config from "@/rainbowKitConfig"
 import { WagmiProvider } from "wagmi"
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import { RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit"
 import { useState } from "react"
+import "@rainbow-me/rainbowkit/styles.css"
 
 export function Providers(prop: {children: ReactNode }) {
-    const [queryClient] = useState(() => new QeryClient())
+    const [queryClient] = useState(() => new QueryClient())
 
     return (
         <WagmiProvider config={config}> 
           <QueryClientProvider client={queryClient}>
             <RainbowKitProvider>
-            {props.children}
+                <ConnectButton/>
+            {prop.children}
             </RainbowKitProvider>
           </QueryClientProvider>
         </WagmiProvider >
