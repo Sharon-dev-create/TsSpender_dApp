fe10cd6fb26781fa52153a86ca7925bf575b7af1
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-06-01 23:20:41 +0100

    tsender airrop form

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -2,11 +2,19 @@
 
  import InputField from "../ui/InputField";
  import { useState } from "react";
+ import { chainsToTSender, tsenderAbi, erc20Abi  } from "../app/constants";
 
  export default function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState("");   
     const [recipient, setRecipient] = useState("");
     const [amount, setAmount] = useState("");
+
+    async function handleSubmit() {
+        // b. If already approved move to step two IF NOT APPROVED, APPROVE FIRST THEN MOVE TO STEP TWO
+        // 1. Approve our tsender contract to send the tokens
+        // 2. Call the tsender contract to send the <tokens></tokens>
+        const tSenderAddress =     
+    }
 
     return (
         <div>
@@ -31,6 +39,9 @@
                 large= {true}
                 onChange={(e) => setAmount(e.target.value)}
             />
+                <button onClick ={handleSubmit}>
+                  Send Tokens
+                </button>
         </div>
     )
  }
