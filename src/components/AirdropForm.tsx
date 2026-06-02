da7f57b9872d3233bf282dde563b89a14fd69d58
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-06-02 13:25:54 +0100

    added addresses

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -3,17 +3,28 @@
  import InputField from "../ui/InputField";
  import { useState } from "react";
  import { chainsToTSender, tsenderAbi, erc20Abi  } from "../app/constants";
+ import { useChainId, useReadContract } from "wagmi"
 
  export default function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState("");   
     const [recipient, setRecipient] = useState("");
     const [amount, setAmount] = useState("");
+    const chainId = useChainId();
+
+    async function getApprovedAmount(TSenderAddress : string |null): Promise<number>{
+        if (!tSenderAddress) {
+            alert("No address found, please use a supported chain")
+            return 0
+        }
+        // read from the chain to see if we have approves enough token
+        // allowance
+    }
 
     async function handleSubmit() {
         // b. If already approved move to step two IF NOT APPROVED, APPROVE FIRST THEN MOVE TO STEP TWO
         // 1. Approve our tsender contract to send the tokens
         // 2. Call the tsender contract to send the <tokens></tokens>
-        const tSenderAddress = chainToTSender[chainId ]
+        const tSenderAddress = chainToTSender[chainId ]["tsender"]
         console.log("chainsToTSender", tSenderAddress);
         console.log(chainId);  
     }
