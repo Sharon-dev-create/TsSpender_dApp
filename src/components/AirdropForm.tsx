ca9bc41df2ec23e359ae05fde29149b41ac62d07
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-06-03 06:55:08 +0100

    adding some functionalities to the airdrop form

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -3,28 +3,38 @@
  import InputField from "../ui/InputField";
  import { useState } from "react";
  import { chainsToTSender, tsenderAbi, erc20Abi  } from "../app/constants";
- import { useChainId, useReadContract } from "wagmi"
-
+ import { useChainId, useReadContract, useAccount } from "wagmi"
+ import { readContract } from "@wagmi/core"; 
+import { config } from "process";
+import { Account } from "viem/tempo";
+ 
  export default function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState("");   
     const [recipient, setRecipient] = useState("");
     const [amount, setAmount] = useState("");
     const chainId = useChainId();
 
-    async function getApprovedAmount(TSenderAddress : string |null): Promise<number>{
+    async function getApprovedAmount(tSenderAddress : string |null): Promise<number>{
         if (!tSenderAddress) {
             alert("No address found, please use a supported chain")
             return 0
         }
-        // read from the chain to see if we have approves enough token
+        // read from the chain to see if we have approved enough token
+        const response = await readContract(config, {
+            abi: erc20Abi,
+            address: tokenAddress as `0x${string}`,
+            fuunctionName: "allowance",
+            args: [account.address, tSenderAddress as `0x${string}`]
+        })
         // allowance
+        return response as number
     }
 
     async function handleSubmit() {
         // b. If already approved move to step two IF NOT APPROVED, APPROVE FIRST THEN MOVE TO STEP TWO
         // 1. Approve our tsender contract to send the tokens
         // 2. Call the tsender contract to send the <tokens></tokens>
-        const tSenderAddress = chainToTSender[chainId ]["tsender"]
+        const tSenderAddress = chainsToTSender[chainId ]["tsender"]
         console.log("chainsToTSender", tSenderAddress);
         console.log(chainId);  
     }
